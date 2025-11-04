d4cb89adad09996f4220ec1cb63e4e632485bd36
Hau Le Trung <anhkhoalez345@gmail.com>
2025-11-04 11:44:05 +0700

    Đổi tên title

--- FashionShop/layouts/header.php
+++ FashionShop/layouts/header.php
@@ -25,7 +25,7 @@ if (getInput('keywork') != '') {
 	<meta name="keywords" content="Ogani, unica, creative, html">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Ogani Shop</title>
+	<title>MiMi shop</title>
 
 	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
