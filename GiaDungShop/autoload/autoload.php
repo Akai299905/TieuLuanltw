d6f50dc4b6614323a181c1ca5e82eb36ae56023e
Hau Le Trung <anhkhoalez345@gmail.com>
2025-11-07 12:01:32 +0700

    Đổi đường dẫn sau khi đổi tên trong autoload

--- GiaDungShop/autoload/autoload.php
+++ GiaDungShop/autoload/autoload.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/../libraries/Function.php';
 
 $db = new Database;
 
-define("ROOT", $_SERVER['DOCUMENT_ROOT'] . "/FashionShop/public/uploads");
+define("ROOT", $_SERVER['DOCUMENT_ROOT'] . "/GiaDungShop/public/uploads");
