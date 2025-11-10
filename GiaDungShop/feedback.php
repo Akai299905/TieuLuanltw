31bd6f17cd326ab5099bfb2ba777dcbe0404de36
Hau Le Trung <anhkhoalez345@gmail.com>
2025-11-10 14:57:21 +0700

    Thây đổi phản hồi

--- GiaDungShop/feedback.php
+++ GiaDungShop/feedback.php
@@ -34,9 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 ?>
 <?php require_once __DIR__ . "/layouts/header.php"; ?>
-<div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49116.39176087041!2d-86.41867791216099!3d39.69977417971648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886ca48c841038a1%3A0x70cfba96bf847f0!2sPlainfield%2C%20IN%2C%20USA!5e0!3m2!1sen!2sbd!4v1586106673811!5m2!1sen!2sbd" height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-</div>
+
 <section class="contact spad">
     <div class="container">
         <div class="row">
@@ -45,16 +43,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                     <div class="section-title">
                         <span>Thông tin</span>
                         <h2>Liên hệ với chúng tôi</h2>
-                        <p>Như bạn có thể mong đợi về một công ty bắt đầu với tư cách là nhà thầu nội thất cao cấp, chúng tôi rất chú ý.</p>
+                        <p>Như bạn có thể mong đợi về một công ty bắt đầu với tư cách là nhà cung cấp hàng gia dụng chất lượng, chúng tôi rất chú ý.</p>
                     </div>
                     <ul>
                         <li>
-                            <h4>America</h4>
-                            <p>195 E Parker Square Dr, Parker, CO 801 <br>+43 982-314-0958</p>
-                        </li>
-                        <li>
-                            <h4>France</h4>
-                            <p>109 Avenue Léon, 63 Clermont-Ferrand <br>+12 345-423-9893</p>
+                            <h4>Vĩnh Long</h4>
+                            <p>41-26/6, 41 Trưng Nữ Vương, Phường 1, Vĩnh Long<br>+84-79-6874808</p>
                         </li>
                     </ul>
                 </div>
